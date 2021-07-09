72d3c35811d4e42c5597ad6305a9530ca1c699df
Hosein-1991 <85505893+Hosein-1991@users.noreply.github.com>
2021-07-09 22:33:21 +0430

    Add files via upload

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 30 ;
+        System.out.println(age);
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
